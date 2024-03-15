2f4273f38ae15b5c118a571e52414b21859aebdc
spongebar-netizen <fatanjrz1@gmail.com>
2024-03-15 10:59:55 +0700

    mengubah kata"langkah 4" menjadi "langkah keempat"

--- tugasalgoritmapart2/tugasalgoritmapart2.cpp
+++ tugasalgoritmapart2/tugasalgoritmapart2.cpp
@@ -1,20 +1,52 @@
-// tugasalgoritmapart2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+int arr[11];
+int n;
+
+void input() {
+	while (true) {
+		cout << "masukkan banyaknya element array = ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+		}
+	}
+	cout << endl;
+	cout << "===========" << endl;
+	cout << "Masukkan beberapa Elemen Array" << endl;
+	cout << "===========" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke berapa-" << (i + 1) << ": ";
+		cin >> arr[i];
+	}
+}
+
+void insertionSort() {
+    int i, n, temp, j;
+    int arr[11];
+
+    for (i = 1; i <= n - 1; i++) //langkah pertama
+    {
+        temp = arr[i]; // lamgkah kedua
+        j = i - 1; //langkah ketiga
+        while (j >= 0 && arr[j] > temp) // langkah keempat
+        {
+            arr[j + 1] = arr[j]; // langkah keempat bagian A
+            j = j - 1; // langkah 4 bagian B
+        }
+        arr[j + 1] = temp; // langkah 5
+
+    }
+
+
+
+}
 
 int main()
 {
     std::cout << "Hello World!\n";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
